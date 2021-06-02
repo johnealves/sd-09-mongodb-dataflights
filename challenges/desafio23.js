c1d668fb90141b26b0aac78a79f6083fcb073652
Pablo Fernandes <pabloprates@gmail.com>
2021-06-02 16:52:26 -0300

    Challenge Twenty Three - reborne

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,12 +1,10 @@
 db.voos.find(
   {
-    litrosCombustivel: { $not: { $gt: 600 } },
-    $nor: [{ "empresa.nome": "GOL" }, { "empresa.nome": "AZUL" }],
+    litrosCombustivel: { $exists: 1, $not: { $gt: 1000 } },
   },
   {
     _id: 0,
     vooId: 1,
-    "empresa.nome": 1,
     litrosCombustivel: 1,
   },
 ).limit(1);
