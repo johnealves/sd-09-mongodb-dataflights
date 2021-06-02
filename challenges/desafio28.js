df0ccca250b06fcd8360991738c0d6369d76cbf3
unknown <nicolasgodek@gmail.com>
2021-06-02 20:18:48 -0300

    fix

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,8 +1,10 @@
+const nome = "LATAM AIRLINES BRASIL";
+
 const counter = db.voos
-  .find({ "empresa.nome": "LATAM AIRLINES BRASIL", natureza: "Doméstica" })
+  .find({ "empresa.nome": nome, natureza: "Doméstica" })
   .count();
 
-db.resumoVoos.insertOne({ empresa: "LATAM AIRLINES BRASIL", totalVoosDomesticos: counter });
+db.resumoVoos.insertOne({ empresa: nome, totalVoosDomesticos: counter });
 
 db.resumoVoos
-  .findOne({ empresa: "LATAM AIRLINES BRASIL" }, { _id: 0, empresa: 1, totalVoosDomesticos: 1 });
+  .findOne({ empresa: nome }, { _id: 0, empresa: 1, totalVoosDomesticos: 1 });
