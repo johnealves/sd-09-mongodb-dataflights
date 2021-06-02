d87088308ac8faaf7dda16e9d966c9e0bce8b7f4
Pablo Fernandes <pabloprates@gmail.com>
2021-06-02 16:48:57 -0300

    Challenge Twenty Four

---
+++ challenges/desafio24.js
@@ -0,0 +1,12 @@
+db.voos.find(
+  {
+    litrosCombustivel: { $not: { $gt: 600 } },
+    $nor: [{ "empresa.nome": "GOL" }, { "empresa.nome": "AZUL" }],
+  },
+  {
+    _id: 0,
+    vooId: 1,
+    "empresa.nome": 1,
+    litrosCombustivel: 1,
+  },
+).limit(1);
