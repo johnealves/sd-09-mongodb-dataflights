2b49975e4b05ed7f026e9f699e24a93b956219ed
edison-ps <edison@osite.com.br>
2021-06-02 17:09:03 -0300

    Desafio 22 V1.00

---
+++ challenges/desafio22.js
@@ -0,0 +1,3 @@
+db.voos.findOne({ "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] },
+"aeroportoOrigem.sigla": "SBGR",
+"aeroportoDestino.sigla": "KJFK" }, { _id: false, vooId: true });
