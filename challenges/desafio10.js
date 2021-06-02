d2d6084fe15c89b9e1c6024e99a90d87971f8c00
LucioOSilva <lucioosilva00@gmail.com>
2021-06-02 15:44:02 -0300

    added req10, adjusts on req5 and req6

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -3,11 +3,13 @@ db.voos.find(
     { "empresa.nome": "GOL" },
     { ano: 2017 },
   ] },
-  { vooId: 1,
-  "empresa.nome": 1,
-  "aeroportoOrigem.nome": 1,
-  "aeroportoDestino.nome": 1,
-  mês: 1,
-  ano: 1,
+  {
+    _id: 0,
+    vooId: 1,
+    "empresa.nome": 1,
+    "aeroportoOrigem.nome": 1,
+    "aeroportoDestino.nome": 1,
+    mês: 1,
+    ano: 1,
   },
-  ).limit(10);
+).limit(10);
