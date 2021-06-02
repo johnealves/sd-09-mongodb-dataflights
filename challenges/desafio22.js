de951c9d8aaf3155a7f92d2f2c6b82bbb6ba3152
jhon <jhonatan.arguello@gmail.com>
2021-06-02 18:18:28 -0300

    Requisito 22.

---
+++ challenges/desafio22.js
@@ -0,0 +1,20 @@
+db.voos.findOne(
+  {
+    $and: [
+      {
+        "empresa.nome": {
+          $in: [
+            "DELTA AIRLINES", "AMERICAN AIRLINES",
+          ],
+        },
+      },
+      {
+        "aeroportoOrigem.sigla": "SBGR",
+      },
+      {
+        "aeroportoDestino.sigla": "KJFK",
+      },
+    ],
+  },
+  { _id: false, vooId: true },
+);
