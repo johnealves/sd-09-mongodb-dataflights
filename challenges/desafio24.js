e2eba1d66d26db84f83d37aebc80841f8aadbb09
tiago-yoneda <xyonedax@gmail.com>
2021-06-02 16:21:01 -0300

    Desafio 24 - completed'

---
+++ challenges/desafio24.js
@@ -0,0 +1,4 @@
+// 24 - Retorne o vooId, empresa.nome e litrosCombustivel do primeiro voo em que litrosCombustivel não seja maior do que 600 e a empresa não seja GOL ou AZUL, e o campo litrosCombustivel exista.
+db.voos.findOne({ "empresa.name": { $nin: ["GOL", "AZUL"] },
+  litrosCombustivel: { $exists: 1, $lt: 600 } },
+  { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 });
