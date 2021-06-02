01e0ce480793eba04159d6853a155f9dcef1f9f1
brenno-calado <brenno.calado@gmail.com>
2021-06-02 16:58:39 -0300

    desafio 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,11 @@
+db.voos.findOne(
+  {
+    "empresa.nome": { $in: ["AMERICAN AIRLINES", "DELTA AIRLINES"] },
+    "aeroportoOrigem.sigla": "SBGR",
+    "aeroportoDestino.sigla": "KJFK",
+  },
+  {
+    _id: 0,
+    vooId: 1,
+  },
+);
