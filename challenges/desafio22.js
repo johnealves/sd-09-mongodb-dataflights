cefa96c3b956952ff96ae4133d5341a0e32e53ff
ThamirysSantos <thamirysgoncalves.prog@gmail.com>
2021-06-02 18:34:16 -0300

    Realizado desafio 22

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,8 +1,6 @@
 db.voos.findOne(
-  {
-    $and: [
-      { $or: [{ empresa: "DELTA AIRLINES" }, { empresa: "AMERICAN AIRLINES" }] },
-      { "aeroportoOrigem.sigla": { $eq: "SBGR" } },
-      { "aeroportoDestino.sigla": { $eq: "KJFK" } }] },
-      { _id: 0, vooId: 1 },
+  { "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] },
+    "aeroportoOrigem.sigla": "SBGR",
+    "aeroportoDestino.sigla": "KJFK" },
+    { _id: 0, vooId: 1 },
 );
