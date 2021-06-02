cf5e473352ce6dc48913cec631cf611412c59b43
Vagner Pereira <sousa.vagner82@gmail.com>
2021-06-02 18:03:11 -0300

    desafio 10

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,18 +1,18 @@
 db.voos.find(
-    {
+    { 
         $and: 
         [
-            { "empresa.nome": "GOL" },
+            { "empresa.nome": "GOL" }, 
             { ano: 2017 },
         ], 
-    },
-    {
-        vooId: 1, 
-        "empresa.nome": 1,
-        "aeroportoOrigem.nome": 1,
-        "aeroportoDestino.nome": 1,
-        mes: 1,
-        ano: 1, 
-        id: 0,
-    },
-).limit(10).pretty();
+    }, 
+            { 
+                vooId: 1, 
+                "empresa.nome": 1, 
+                "aeroportoOrigem.nome": 1, 
+                "aeroportoDestino.nome": 1, 
+                mes: 1, 
+                ano: 1, 
+                _id: 0, 
+            },
+            ).limit(10).pretty();
