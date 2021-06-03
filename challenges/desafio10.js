86557efa4f7edd6f6ec14481e0df99464c9a975e
Tiago de Almeida Paz <tiagopaz.dev@gmail.com>
2021-06-03 15:25:41 -0300

    req 08, 09, 10, 11, 12, 13 and 14

---
+++ challenges/desafio10.js
@@ -0,0 +1,8 @@
+db.voos.find({ ano: { $eq: 2017 }, "empresa.nome": "GOL" }, {
+  _id: false,
+  vooId: true,
+  "empresa.nome": true,
+  "aeroportoOrigem.nome": true,
+  "aeroportoDestino.nome": true,
+  mes: true,
+  ano: true }).limit(10);
