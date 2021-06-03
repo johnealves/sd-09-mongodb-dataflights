ae2e08ab326022b781c9034c3902079ead7284cc
Rafael Reis <rafaelrnascimento2006@gmail.com>
2021-06-03 00:39:16 -0300

    organizando codigo

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,8 +1,5 @@
-db.voos.findOne(
-  {
+db.voos.findOne({
     "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] },
     "aeroportoOrigem.sigla": { $eq: "SBGR" },
     "aeroportoDestino.sigla": { $eq: "KJFK" },
-  },
-  { vooId: 1, _id: 0 },
-);
+  }, { vooId: 1, _id: 0 });
--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -1,7 +1,4 @@
-db.voos.findOne(
-  {
+db.voos.findOne({
     litrosCombustivel: { $not: { $gt: 600 }, $exists: true },
     "empresa.nome": { $nin: ["GOL", "AZUL"] },
-  },
-  { vooId: 1, "empresa.nome": 1, litrosCombustivel: 1, _id: 0 },
-);
+  }, { vooId: 1, "empresa.nome": 1, litrosCombustivel: 1, _id: 0 });
