9b36be10845153052fea6e0e6dc1396a0829fd25
Nathália <nathaliaveneziano@icloud.com>
2021-06-03 00:00:45 -0300

    FEAT - Requisito 22

---
+++ challenges/desafio22.js
@@ -0,0 +1,17 @@
+db.voos
+.find(
+  {
+    $and: [
+      {
+        $or: [
+          { "empresa.nome": "AMERICAN AIRLINES" },
+          { "empresa.nome": "DELTA AIRLINES" },
+        ],
+      },
+      { "aeroportoOrigem.sigla": "SBGR" },
+      { "aeroportoDestino.sigla": "KJFK" },
+    ],
+  },
+  { _id: 0, vooId: 1 },
+)
+.limit(1);
