c7351307dcd272c7140463e9e2cf611167ad45a1
Diogenes Santos <diogenesfelipe.s@outlook.com>
2021-06-04 20:47:34 -0300

    fix/Desafio23

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -2,7 +2,7 @@ db.voos.findOne(
   {
     $and:[
       { litrosCombustivel: { $lte: 1000 } },
-      {litrosCombustivel: { $exists: true } },
+      { litrosCombustivel: { $exists: true } },
     ],
   },
   { vooId: 1, litrosCombustivel: 1, _id: 0 }
