36b47f0a66aa2b5354984d265f4c89f10ac72a47
Diogenes Santos <diogenesfelipe.s@outlook.com>
2021-06-04 18:24:55 -0300

    fix/Desafio22

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -4,12 +4,12 @@ db.voos.findOne(
       { 
         $or: [
           { "empresa.nome": "DELTA AIRLINES" },
-          { "empresa.nome": "AMERICAN AIRLINES"}
+          { "empresa.nome": "AMERICAN AIRLINES" }
         ],
       },
       { "aeroportoOrigem.sigla": "SBGR" },
-      {"aeroportoDestino.sigla": "KJFK"}
-    ]
+      { "aeroportoDestino.sigla": "KJFK" },
+    ],
   },
   { vooId: 1, _id: 0 },
 );
