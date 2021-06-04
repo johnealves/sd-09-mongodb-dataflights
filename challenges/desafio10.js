d4868dac5822188f7efdd90dc2402a22d5d966a2
BBrolesi <brunohsbrolesi@gmail.com>
2021-06-03 21:01:36 -0300

    fix: lint issues

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,7 +1,7 @@
 db.voos.find(
     {
         ano: 2017, 
-        "empresa.nome": "GOL"
+        "empresa.nome": "GOL",
     }, 
     {
         _id: 0,
@@ -10,6 +10,6 @@ db.voos.find(
         "aeroportoOrigem.nome": 1,
         "aeroportoDestino.nome": 1,
         mes: 1,
-        ano: 1
+        ano: 1,
     },
 ).limit(10);
