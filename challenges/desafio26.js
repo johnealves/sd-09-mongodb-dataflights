805760899c5c1a2b5ae3345e5563b157df4e5011
Lucas Silva <martssilvapro@gmail.com>
2021-06-04 16:59:00 -0300

    corrige desafio 26

--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -1,1 +1,5 @@
-db.voos.deleteMany({ "empresa.nome": { $eq: "GOL" }, "passageiros.pagos": { $in: [5, 10] } });
+db.voos.deleteMany(
+  { "empresa.nome": { $eq: "GOL" },
+  "passageiros.pagos": { $in: [5, 10] },
+  $and: [{ "passageiros.pagos": { $eq: 5 } }, { "passageiros.pagos": { $eq: 10 } }] },
+);
