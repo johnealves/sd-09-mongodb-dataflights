a090ce57bab2f6694b65f3f83c90b782aa65731e
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-06-05 19:25:20 -0300

    Requisito 6

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,1 +1,7 @@
-db.voos.find({ vooId: 756807 }, { _id: 0, "empresa.sigla": true, "empresa.nome": 1, passageiros: 1 });
+db.voos.find({ vooId: 756807 },
+  {
+    _id: 0,
+    "empresa.sigla": true,
+    "empresa.nome": 1,
+    passageiros: 1,
+  });
