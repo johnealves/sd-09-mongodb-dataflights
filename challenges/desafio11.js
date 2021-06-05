4cd53d36575727bcc31f4c4b29e63dd0b19f4a03
negres <nataliaknegres@gmail.com>
2021-06-05 15:40:22 -0300

    Corrige requisitos 10 e 11 - Add requisito 12

--- challenges/desafio11.js
+++ challenges/desafio11.js
@@ -1,7 +1,2 @@
-db.voos.find({ "aeroportoDestino.pais": {
-  $in: [
-    "BRASIL",
-    "ARGENTINA",
-    "CHILE",
-  ],
-} }).count();
+db.voos.find({
+  "aeroportoDestino.pais": { $ne: "ESTADOS UNIDOS" } });
