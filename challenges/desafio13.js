d995ebd92a63800ee11dd1cc1e2d505764b3a906
Leonardo José da Silveira <rpgleo.js@gmail.com>
2021-06-04 22:21:33 -0300

    19 attempt

--- challenges/desafio13.js
+++ challenges/desafio13.js
@@ -1,3 +1,3 @@
 db.voos.count(
-  { "aeroportoDestino.continente": { $not: { $in: ["EUROPA", "ÁSIA", "EUROPA"] } } },
+  { "aeroportoDestino.continente": { $not: { $in: ["EUROPA", "ÁSIA", "OCEANIA"] } } },
 );
