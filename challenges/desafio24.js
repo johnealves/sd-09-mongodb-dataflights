b12654ec909c371fce2f44832264b5531904cd11
Diogenes Santos <diogenesfelipe.s@outlook.com>
2021-06-04 21:10:29 -0300

    feat/Desafio24

---
+++ challenges/desafio24.js
@@ -0,0 +1,10 @@
+db.voos.findOne(
+  {
+    $and: [
+      { litrosCombustivel: { $lte: 600 } },
+      { "empresa.nome": { $ne: "AZUL" } },
+      { "empresa.nome": { $ne: "GOL" } },
+      { litrosCombustivel: { $exists: true } },
+    ], 
+  },
+  { vooId: 1, "empresa.nome": 1, litrosCombustivel: 1, _id: 0 });
