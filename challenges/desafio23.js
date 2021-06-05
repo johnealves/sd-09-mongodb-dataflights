5828d8f4a82a965b07938b83c16bc79d4c619abe
santiago <felipesantiagolds@gmail.com>
2021-06-05 11:28:08 -0300

    Requisito 23

---
+++ challenges/desafio23.js
@@ -0,0 +1,8 @@
+db.voos.findOne({
+  $and: [
+    {
+      litrosCombustivel: { $not: { $gt: 1000 } } },
+    {
+      litrosCombustivel: { $exists: true } }] },
+  {
+    _id: false, vooId: true, litrosCombustivel: true });
