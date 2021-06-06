72880b85b1fab34499b0a79fbeb208e9b2b8a6bc
Pedro Henrique <pedroenrique.400@gmail.com>
2021-06-06 13:33:16 -0300

    req 23

---
+++ challenges/desafio23.js
@@ -0,0 +1,15 @@
+db.voos.findOne(
+    { 
+      $and:
+      [
+        { $nor:
+          [
+            { litrosCombustivel: { $gt: 1000 } },
+          ],
+        },
+        {
+          litrosCombustivel: { $exists: true },
+        },
+      ],
+    }, { _id: 0, vooId: 1, litrosCombustivel: 1 },
+    );
