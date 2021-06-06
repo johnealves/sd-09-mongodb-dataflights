20c30bdf61324db29608a1a8a1009ccd519dbc2f
jhon <jhonatan.arguello@gmail.com>
2021-06-05 21:22:15 -0300

    Reajustando o requisito 27.

--- challenges/desafio27.js
+++ challenges/desafio27.js
@@ -1,14 +1,12 @@
-db.resumoVoos.insertOne(
-  {
-    empresa: "PASSAREDO",
-    totalVoosDosmesticos: db.voos.count({
-      $and: [
-        { "empresa.nome": "PASSAREDO" },
-        { natureza: "Doméstica" },
-      ],
-    }),
-  },
-);
+db.resumoVoos.insertOne({
+  empresa: "PASSAREDO",
+  totalVoosDosmesticos: db.voos.count({
+    $and: [
+      { "empresa.nome": "PASSAREDO" },
+      { natureza: "Doméstica" },
+    ],
+  }),
+});
 
 db.resumoVoos.findOne(
   { empresa: "PASSAREDO" },
