e01273ee10f6adea2157852088d8955c4828c1a7
Rafael Pufal <rafael.pufal23@gmail.com>
2021-06-07 14:24:45 -0300

    req 27

---
+++ challenges/desafio27.js
@@ -0,0 +1,5 @@
+db.resumoVoos.insertOne({
+  empresa: "PASSAREDO",
+  totalVoosDomesticos: db.voos.count({ natureza: "Doméstica", "empresa.nome": "PASSAREDO" }),
+});
+db.resumoVoos.findOne({ empresa: "PASSAREDO" }, { _id: 0 });
