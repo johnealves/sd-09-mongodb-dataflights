1508e2e3a8077305e3ed829a29869efae1d51e10
Emanoel Leão <emanuelleaomarinho@gmail.com>
2021-06-09 01:16:56 -0300

    Task 24 - LINT Arquivo /challenges/desafio24.js   Linha 4: Multiple spaces found before '}'.   Linha 6: A space is required after '{'.   Linha 6: Missing space before value for key 'vooId'.   Linha 6: Missing space before value for key 'litrosCombustivel'.   Linha 6: A space is required before '}'.

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -1,7 +1,7 @@
 db.voos.findOne(
   {
     litrosCombustivel: { $exists: true, $lte: 600 },
-    "empresa.nome": { $nin: ["GOL", "AZUL"]  },
+    "empresa.nome": { $nin: ["GOL", "AZUL"] },
   },
-  {_id: 0, vooId:1, "empresa.nome": 1, litrosCombustivel:1},
+  { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 },
 );
