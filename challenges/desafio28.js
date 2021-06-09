8dba38ea47df79b81cbf547064fe9ce4779b434c
fernandacajueiro <fernandacajueiromachado@gmail.com>
2021-06-09 13:42:05 -0300

    Requirements 27 and 28 + code correction

--- challenges/desafio28.js
+++ challenges/desafio28.js
@@ -1,7 +1,13 @@
+const empresa = "LATAM AIRLINES BRASIL";
+
 db.resumoVoos.insertOne({
-  empresa: "LATAM AIRLINES BRASIL",
+  empresa,
   totalVoosDomesticos: db.voos.count(
       { "empresa.nome": "LATAM AIRLINES BRASIL", natureza: "Doméstica" },
   ),
 });
 
+db.resumoVoos.findOne(
+  { empresa },
+  { _id: 0, empresa: 1, totalVoosDomesticos: 1 },
+); 
