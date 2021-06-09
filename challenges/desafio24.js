81fe69b973013855c3a7151ba5eddc5cbcab372e
Uibirá <usbarreto@gmail.com>
2021-06-09 12:26:22 -0300

    Corrige eslint no D24

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -3,5 +3,5 @@ db.voos.find(
     litrosCombustivel: { $lt: 600, $exists: 1 },
     "empresa.nome": { $nin: ["GOL", "AZUL"] },
   },
-  {  _id: 0, vooId: 1, litrosCombustivel: 1, "empresa.nome": 1 },
+  { _id: 0, vooId: 1, litrosCombustivel: 1, "empresa.nome": 1 },
 ).limit(1);
