c6e091bf653e15639017b507adf8a44ce61574eb
Leticia Lima <letylima96@gmail.com>
2021-06-08 21:39:36 -0300

    desafio 27

---
+++ challenges/desafio27.js
@@ -0,0 +1,18 @@
+const valorTotal = db.voos.count({
+  "empresa.nome": "PASSAREDO",
+  natureza: "Doméstica",
+});
+db.resumoVoos.insertOne({
+  empresa: "PASSAREDO",
+  totalVoosDomesticos: valorTotal,
+});
+db.resumoVoos.findOne(
+  {
+    empresa: "PASSAREDO",
+  },
+  {
+    _id: 0,
+    empresa: 1,
+    totalVoosDomesticos: 1,
+  },
+);
