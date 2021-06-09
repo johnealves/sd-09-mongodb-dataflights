beebad1407dbc9040fcdb1f80957702b56d5d7fd
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-06-09 14:18:11 -0300

    desafio 22

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,14 +1,12 @@
 db.voos.find(
-    { 
-        "empresa.nome": "GOL", ano: 2017,
-    },
-    {
+    { ano: 2017, "empresa.nome": "GOL" },
+      {
         _id: 0,
-        voodId: 1,
+        vooId: 1,
         "empresa.nome": 1,
         "aeroportoOrigem.nome": 1,
         "aeroportoDestino.nome": 1,
         mes: 1,
         ano: 1,
-    },
-    ).limit(10);
+      },
+  ).limit(10);
