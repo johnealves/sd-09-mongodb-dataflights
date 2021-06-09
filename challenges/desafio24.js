13f47de06920b172552cfbede99a5a8ccfd8a6ab
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-06-09 14:55:34 -0300

    desafio 24

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -1,20 +1,13 @@
-db.voos.findOne(
-    { 
-      $nor: [
-        {
-          litrosCombustivel: { $gt: 600 },
-        },
-        {
-          litrosCombustivel: { $exists: false },
-        },
-        {
-          "empresa.nome": { $in: ["GOL", "AZUL"] },
-        },
-      ],
-    },  
-    { 
-      _id: 0,
-      "empresa.nome": 1,
-      litrosCombustivel: 1,
-    },
-  );
+db.voos.findOne({
+    $nor: [
+        { litrosCombustivel: { $exists: false } },
+        { litrosCombustivel: { $gt: 600 } },
+        { "empresa.nome": { $in: ["GOL", "AZUL"] } },
+    ],
+    }, 
+    {
+        _id: 0,
+        vooId: 1,
+        "empresa.nome": 1,
+        litrosCombustivel: 1,
+    });
