5c00c95542035c266a4607f98df8001789b98f94
Arthur Nunes <coletivosuburbano@gmail.com>
2021-06-10 20:43:17 -0300

    ex 21 ao 26

---
+++ challenges/desafio22.js
@@ -0,0 +1,10 @@
+db.voos
+  .find(
+    {
+      "empresa.nome": { $in: ["DELTA AIRLINES", "AMERICAN AIRLINES"] },
+      "aeroportoOrigem.sigla": "SBGR",
+      "aeroportoDestino.sigla": "KJFK",
+    },
+    { _id: false, vooId: true },
+  )
+  .limit(1);
