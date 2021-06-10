4064d9509c3424a1e2f9f2af872f7873ef4477e2
mayconfelipes <mayconfelipe.dev@gmail.com>
2021-06-10 00:37:36 -0300

    Requisitos 21 ao 23

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -7,6 +7,6 @@ db.voos
       "aeroportoOrigem.sigla": "SBGR",
       "aeroportoDestino.sigla": "KJFK",
     },
-    { vooId: 1, _id: 0 }
+    { vooId: 1, _id: 0 },
   )
   .limit(1);
--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -3,9 +3,9 @@ db.voos
     {
       $and: [
         { litrosCombustivel: { $exists: true } },
-        { litrosCombustivel: { $gt: 1000 } },
+        { litrosCombustivel: { $lt: 1000 } },
       ],
     },
-    { vooId: 1, litrosCombustivel: 1, _id: 0 }
+    { vooId: 1, litrosCombustivel: 1, _id: 0 },
   )
   .limit(1);
