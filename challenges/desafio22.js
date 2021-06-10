6dcbdd6a8dc897ea46ff95383ffd2f07e41f1cf9
mayconfelipes <mayconfelipe.dev@gmail.com>
2021-06-10 00:34:05 -0300

    Requisitos 21 ao 23

---
+++ challenges/desafio22.js
@@ -0,0 +1,12 @@
+db.voos
+  .find(
+    {
+      $or: [
+        { "empresa.nome": { $in: ["AMERICAN AIRLINES", "DELTA AIRLINES"] } },
+      ],
+      "aeroportoOrigem.sigla": "SBGR",
+      "aeroportoDestino.sigla": "KJFK",
+    },
+    { vooId: 1, _id: 0 }
+  )
+  .limit(1);
