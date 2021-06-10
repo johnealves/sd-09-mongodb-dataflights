29b41c503e65aac3ef61909f707b9a41808b4f01
mayconfelipes <mayconfelipe.dev@gmail.com>
2021-06-10 15:52:09 -0300

    Requisitos 24 e 25. Fixing Lint

--- challenges/desafio24.js
+++ challenges/desafio24.js
@@ -7,6 +7,6 @@ db.voos
         { "empresa.nome": { $nin: ["GOL", "AZUL"] } },
       ],
     },
-    { vooId: 1, "empresa.nome": 1, litrosCombustivel: 1, _id: 0 }
+    { vooId: 1, "empresa.nome": 1, litrosCombustivel: 1, _id: 0 },
   )
   .limit(1);
