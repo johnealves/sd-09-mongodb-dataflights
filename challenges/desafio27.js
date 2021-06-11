6da971a30027d4a97103376cfe7004044e3b2793
Arthur Nunes <coletivosuburbano@gmail.com>
2021-06-11 07:42:25 -0300

    ex 27 e 28

---
+++ challenges/desafio27.js
@@ -0,0 +1,8 @@
+db.resumoVoos.insertOne({
+  empresa: "PASSAREDO",
+  totalVoosDomesticos: db.voos
+    .find({ natureza: "Doméstica", "empresa.nome": "PASSAREDO" })
+    .count(),
+});
+
+db.resumoVoos.find({ empresa: "PASSAREDO" }, { _id: false }).limit(1);
