7f42a88c12337185db19d16cdd8445cb6a074c92
diego-vini <analistamm@gmail.com>
2021-06-26 10:58:56 -0300

    Desafios 21 ao 26

--- challenges/desafio22.js
+++ challenges/desafio22.js
@@ -1,5 +1,5 @@
-db.voos.find({$and: [
-    {$or: [{ "empresa.nome": "DELTA AIRLINES"}, {"empresa.nome": "AMERICAN AIRLINES" }]},
+db.voos.find({ $and: [
+    { $or: [{ "empresa.nome": "DELTA AIRLINES" }, { "empresa.nome": "AMERICAN AIRLINES" }] },
     { "aeroportoOrigem.sigla": "SBGR" },
-    { "aeroportoDestino.sigla": "KJFK" }
-]}, { _id: 0, vooId: 1 }).limit(1);
+    { "aeroportoDestino.sigla": "KJFK" },
+] }, { _id: 0, vooId: 1 }).limit(1);
